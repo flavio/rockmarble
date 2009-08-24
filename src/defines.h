fc33c38f5fcc6c4f1fb797d62aa37f6f844984c1
Flavio Castelli <fcastelli@novell.com>
2009-08-24 15:25:00 +0200

    upgraded to 0.6.1

--- src/defines.h
+++ src/defines.h
@@ -21,7 +21,7 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define ROCKMARBLE_VERSION "0.6.0"
+#define ROCKMARBLE_VERSION "0.6.1"
 #define API_KEY "58bc400e1ec91885c167573d770efc1e"
 
 #endif // DEFINES_H

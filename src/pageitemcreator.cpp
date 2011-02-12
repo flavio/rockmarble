2b3bc043af64b7aa05ba23452074c445bd5b1c0c
Flavio Castelli <fcastelli@novell.com>
2011-02-12 16:11:03 +0100

    Show icons + explainations on the main page.

--- src/pageitemcreator.cpp
+++ src/pageitemcreator.cpp
@@ -7,4 +7,22 @@ void PageItemCreator::updateCell(const QModelIndex &index, MWidget *cell) const
 
   contentItem->setTitle(pages.value((MainPage::PageType) data.toInt()));
   contentItem->boundingRect();
+  switch ((MainPage::PageType) data.toInt()) {
+    case MainPage::ByArtist:
+      contentItem->setImageID("icon-m-content-artist");
+      contentItem->setSubtitle("Search the events by artist and then by country.");
+      break;
+    case MainPage::ByLocation:
+      contentItem->setImageID("icon-m-clock-city-create");
+      contentItem->setSubtitle("Search the events by country and then by artist.");
+      break;
+    case MainPage::ByCurrentLocation:
+      contentItem->setImageID("icon-m-common-location");
+      contentItem->setSubtitle("Show all the events taking place near you.");
+      break;
+    case MainPage::ByStar:
+      contentItem->setImageID("icon-m-common-favorite-mark");
+      contentItem->setSubtitle("Show all the events you have starred.");
+      break;
+    }
 }
